2210740bca18009e740a6bc901e37196ea7346d5
Lucian Amos <locamos@gmail.com>
2019-12-07 16:44:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nestedTarget(){
 function increaseRankBy(n){
   var scores = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for (let i = 0; i<scores.length; i++){
-    scores[i].innerHTML += 3;
+    scores[i].innerHTML = 4;
   }
 }
 
