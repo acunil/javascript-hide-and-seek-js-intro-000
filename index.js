0a9f023c069f4508c50b1f10be4078a46d4c54ab
Lucian Amos <locamos@gmail.com>
2019-12-07 16:40:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector){
+  var f = document.querySelector(selector);
+  return f;
+}
 
+function nestedTarget(){
+  var j = document.getElementById('nested').querySelector('div.target');
+  return j;
+}
+
+function increaseRankBy(n){
+  var scores = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i<scores.length; i++){
+    scores[i].innerHTML = scores[i]+n;
+  }
+}
+
+
+function deepestChild(){
+  
+}
